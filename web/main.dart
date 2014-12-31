222b2c3cb50920acfcf56273597bd7dd9b742e0e
Viktor Dakalov <atlantidu@gmail.com>
2014-12-31 16:32:10 +0500

    Some fixes

--- web/main.dart
+++ web/main.dart
@@ -1,15 +1,26 @@
 
 import "dart:html";
+import "dart:async";
 import "package:FigureEditor/FigureEditor.dart";
 
 main() {
 
-  CanvasElement canvas = new CanvasElement(width: 1000, height: 600);
+  CanvasElement canvas = new CanvasElement(width: 500, height: 450);
   CanvasRenderingContext2D context = canvas.getContext("2d");
   document.body.append(canvas);
   canvas.tabIndex = 1;
   canvas.focus();
 
+  DivElement fpsLabel = new DivElement();
+  document.body.append(fpsLabel);
+
   FigureEditor fe = new FigureEditor.fromCanvas(canvas);
+
+  new Timer(new Duration(seconds: 5), (){
+    canvas..width = 1000
+          ..height = 600;
+
+    fe.frame.update();
+  });
 
 }
